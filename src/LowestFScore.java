59089736450d14dc78506a7483044c746b9ebc7a
Ivan Mehandzhiev <igmehandzhiev@gmail.com>
2017-11-09 23:25:47 +0200

    Add @NotNull Annotation for Comparator
    Signed-off-by: Ivan Mehandzhiev <igmehandzhiev@gmail.com>

--- src/LowestFScore.java
+++ src/LowestFScore.java
@@ -1,13 +1,15 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Comparator;
 
 /**
  * @author <a href = "mailto:imehandzhiev@intracol.com">imehandzhiev</a>
  * @since 08-Nov-17
  */
-public class LowestFScore implements Comparator<State> {
+class LowestFScore implements Comparator<State> {
 
     @Override
-    public int compare(State o1, State o2) {
+    public int compare(@NotNull State o1, @NotNull State o2) {
         return Integer.compare(o1.getfScore(), o2.getfScore());
     }
 }

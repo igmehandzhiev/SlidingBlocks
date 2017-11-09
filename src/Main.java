9f56b6e2186c9efea91703fb1474adda35a2fec1
Ivan Mehandzhiev <igmehandzhiev@gmail.com>
2017-11-09 23:44:29 +0200

    Main UI for testing
    Signed-off-by: Ivan Mehandzhiev <igmehandzhiev@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,29 @@
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author <a href = "mailto:imehandzhiev@intracol.com">imehandzhiev</a>
  * @since 06-Nov-17
  */
 public class Main {
     public static void main(String[] args) {
-//        final int length = 8 + 1;
-//        final int puzzleSize = (int) Math.sqrt(8);
-//        int[][] initPuzzle = new int[puzzleSize + 1][puzzleSize + 1];
-//        HashSet<Integer> numbers = new HashSet<>(length);
-//
-//        for (int x = 0; x <= puzzleSize; ++x) {
-//            for (int y = 0; y <= puzzleSize; ++y) {
-//                int random = new Random().nextInt(length);
-//                while (numbers.contains(random)) {
-//                    random = new Random().nextInt(length);
-//                }
-//                initPuzzle[x][y] = random;
-//                numbers.add(random);
-//            }
-//        }
-//
-//        for (int row = 0; row < initPuzzle.length; row++) {
-//            for (int col = 0; col < initPuzzle[row].length; col++) {
-//                System.out.println("X: " + row + " Y: " + col + " NUMBER: " + initPuzzle[row][col]);
-//                // Do stuff
-//            }
-//        }
-//        int[][] current = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
-//        int[][] childState = new int[current.length][];
-//        for (int i = 0; i < current.length; i++) {
-//            int[] tempMatrix = current[i];
-//            int tempLength = tempMatrix.length;
-//            childState[i] = new int[tempLength];
-//            System.arraycopy(tempMatrix, 0, childState[i], 0, tempLength);
-//        }
-//
-//        for (int[] row :
-//                childState) {
-//            for (int num :
-//                    row) {
-//                System.out.println(num);
-//            }
-//        }
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        while ((n < 4) || ((Math.sqrt(n + 1) % 2 != 1.0) && (Math.sqrt(n + 1) % 2 != 0.0))) {
+            System.out.println((Math.sqrt(n + 1) % 2 != 1.0));
+            System.out.println((Math.sqrt(n + 1) % 2));
+            System.out.println("Type proper number: ");
+            n = input.nextInt();
+        }
+        input.close();
+
+        final List<State> path = SlidingBlocksApp.aStar(n);
+
+        System.out.println("Path Size is: " + path.size());
+        for (State state :
+                path) {
+            System.out.println(state);
+        }
     }
 }
